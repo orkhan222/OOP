e1213c2141b8c43af1f4fbc776d86361da9f77af
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2021-11-28 19:29:56 +0400

    first commit

---
+++ a.py
@@ -0,0 +1,38 @@
+# num1 = int(input('reqem: '))
+# new = []
+# num2 = 0
+
+# while num1>0:
+#     num2=num1%10
+#     new.append(num2)
+#     num1//=10
+    
+# print(max(new))
+
+# num1 = int(input('reqem: '))
+# num2 = int(input('reqem: '))
+# count = 0
+# while num1 >= num2:
+#     if num1%num2 == 0:
+#         count+=1
+#     num2+=1 
+# print(count)
+
+# a = int(input('reqem: '))
+# b= []
+# c = 0
+# while a>0:
+    
+#     b.append(c)
+    
+# print(max(b))
+
+
+# print(dir(float))
+
+urunA=5000
+urunB=6000
+kdv=0.18
+
+print(urunA + (urunA*kdv))
+print(urunB + (urunB*kdv))
