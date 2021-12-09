fd20420b72868d008a272d54533dc074cc440c6a
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2021-12-09 18:03:30 +0400

    Data-types-demo

---
+++ data-types-demo.py
@@ -0,0 +1,30 @@
+# Daire Alani : pi r kvaritarti
+# Daire Cevre : 2 pi r
+#  Yari capi verilen bir dairenin alan ve ceversini hesaplayiniz. (pi: 3.14)
+
+
+r = float(input('radius: '))
+
+pi = 3.14
+
+dairelani= pi * (r **2)
+Dairecevre = 2 * pi * r
+print(Dairecevre)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Bir aracin km cinsinden gittigi yol bilgisini mil olarak yazdiriniz.
+# mil = km / 1.609344
