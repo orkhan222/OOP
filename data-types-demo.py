77d9a28d2ed8e4f660996d40c4e2c5b7165b2f76
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2021-12-13 19:47:41 +0400

    data-types 2-ci sual yazildi

--- data-types-demo.py
+++ data-types-demo.py
@@ -3,28 +3,19 @@
 #  Yari capi verilen bir dairenin alan ve ceversini hesaplayiniz. (pi: 3.14)
 
 
-r = float(input('radius: '))
+# r = float(input('radius: '))
 
-pi = 3.14
+# pi = 3.14
 
-dairelani= pi * (r **2)
-Dairecevre = 2 * pi * r
-print(Dairecevre)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# dairelani= pi * (r **2)
+# Dairecevre = 2 * pi * r
+# print(Dairecevre)
 
 
 # Bir aracin km cinsinden gittigi yol bilgisini mil olarak yazdiriniz.
-# mil = km / 1.609344
+# mil = km / 1.6093444
+mesafeKm = input()
+mesafeMil = float(mesafeKm) / 1.6093444
+
+result = str(mesafeKm) + ' km =  ' + str(mesafeMil) + ' mil'
+print(result)
