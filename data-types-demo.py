8da5e9e2e4db57249b9dde064935d69a473fca1f
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2021-12-14 10:48:36 +0400

    List yazildi

--- data-types-demo.py
+++ data-types-demo.py
@@ -3,13 +3,13 @@
 #  Yari capi verilen bir dairenin alan ve ceversini hesaplayiniz. (pi: 3.14)
 
 
-# r = float(input('radius: '))
+r = float(input('radius: '))
 
-# pi = 3.14
+pi = 3.14
 
-# dairelani= pi * (r **2)
-# Dairecevre = 2 * pi * r
-# print(Dairecevre)
+dairelani= pi * (r **2)
+Dairecevre = 2 * pi * r
+print(Dairecevre)
 
 
 # Bir aracin km cinsinden gittigi yol bilgisini mil olarak yazdiriniz.
