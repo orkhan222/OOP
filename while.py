f19cb82ef24d330d1f6342ac87a55d38fbcfa156
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2022-01-04 18:20:29 +0400

    while

--- while.py
+++ while.py
@@ -1,6 +1,6 @@
-num1 = int(input('Num1: '))
-# new = []
-num2 = 0
+# num1 = int(input('Num1: '))
+# # new = []
+# num2 = 0
 
 # while num1> 0:
 #     num2 = num1%10
@@ -15,7 +15,18 @@ num2 = 0
 # print(new)
 
 
-while num1>0:
-    num2 = (num2*10) + (num1%10)
-    num1//=10
-print(num2)
+# while num1>0:
+#     num2 = (num2*10) + (num1%10)
+#     num1//=10
+# print(num2)
+
+
+num1 = 12
+name = 'orkhan'
+liste = [1,'2',3,4,5,6,'orkhan',['orkhan']]
+index = 0
+
+while len(liste) > index:
+    print(liste[index])
+    index+=1
+    
