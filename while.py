7aa1ea62f36cd51db4b739a1481835201d032b91
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2022-01-05 09:09:41 +0400

    while

--- while.py
+++ while.py
@@ -21,12 +21,27 @@
 # print(num2)
 
 
-num1 = 12
-name = 'orkhan'
-liste = [1,'2',3,4,5,6,'orkhan',['orkhan']]
-index = 0
+# num1 = 12
+# name = 'orkhan'
+# liste = [1,'2',3,4,5,6,'orkhan',['orkhan']]
+# index = 0
 
-while len(liste) > index:
-    print(liste[index])
+# while len(liste) > index:
+#     print(liste[index])
+#     index+=1
+    
+    
+    
+    
+adlar = ['orkhan','eli','veli','salman','muxtar']
+index = 0
+yadda = ''
+while len(adlar) > index:
+    if index != len(adlar)-1:
+        yadda = adlar[index]
+        adlar[index] = adlar[index+1]
+        adlar[index+1] = yadda
     index+=1
+    
+print(adlar)
     
